20abe1789b48db514a9ca6e5cb6ac9077b1dd557
Ryan Furman <rfurman3803l@gmail.com>
2025-11-27 17:03:05 -0600

    Restructure for proper init

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,46 +1,38 @@
-resource "aws_iam_role" lambda_cert_issuer" {
-    name = "lambda_cert_issuer_role"
+# Trust policy allowing Lambda to assume these roles
+data "aws_iam_policy_document" "lambda_trust" {
+  statement {
+    effect = "Allow"
 
-    assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
-}
-
-    resource "aws_iam_role" "lambda_scanner" {
-        name - "lambda_scanner_role"
-
-        assume_role_policy = data.aws_iam_policy.lambda_trust.json
-    }
-
-    resource "aws_iam_role" "lambda_remediation" {
-        name = "lambda_remediation_role"
-
-    assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
-}
-
-resource "aws_iam_role" "pca_admin" {
-    name = "pca_admin_role"
-
-    assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
-}
-
-resoure "aws_iam_role" "pqc_keygen" {
-    name = "pqc_keygen_role"
-
-    assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
-}
-
-data "aws_iam_policy_documents" lambda_trust" {
-    statement {
-        effect = "Allow"
-
-        principals {
-        type    = "Service"
-        identifiers = ["lambda.amazonaws.com"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
     }
 
     actions = ["sts:AssumeRole"]
-    }
+  }
 }
 
-module "iam" {
-    source = "../../modules/iam"
+resource "aws_iam_role" "lambda_cert_issuer" {
+  name               = "lambda_cert_issuer_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+}
+
+resource "aws_iam_role" "lambda_scanner" {
+  name               = "lambda_scanner_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+}
+
+resource "aws_iam_role" "lambda_remediation" {
+  name               = "lambda_remediation_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+}
+
+resource "aws_iam_role" "pca_admin" {
+  name               = "pca_admin_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+}
+
+resource "aws_iam_role" "pqc_keygen" {
+  name               = "pqc_keygen_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
 }
